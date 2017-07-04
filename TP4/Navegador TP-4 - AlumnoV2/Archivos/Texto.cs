a02f4a5633b922f1bfe2265c643a7ef5497209b3
guidoves <guido.vespignani@gmail.com>
2017-07-03 23:34:56 -0300

    commit

---
+++ TP4/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string archivo;
+
+        public Texto(string archivo)
+        {
+            this.archivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter s = new StreamWriter(this.archivo,true);
+                s.WriteLine(datos);
+                s.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            try
+            {
+                StreamReader r = new StreamReader(this.archivo);
+  
+                
+                while (!r.EndOfStream)
+                {
+                    datos.Add(r.ReadLine());
+                }
+
+                r.Close();
+                return true;
+            }
+            catch (Exception )
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TP4/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public delegate void ProgresoCarga(int progreso);
+        public delegate void Descarga(string descarga);
+
+        public event ProgresoCarga pCarga;
+        public event Descarga descarga;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged ;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted ;
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);  
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.pCarga(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.html = e.Result;
+                this.descarga(this.html);
+            }
+            catch (Exception ex)
+            {
+                this.descarga(ex.InnerException.Message);
+            }
+        }
+    }
+}
